92611f64b686b1206dc802e5875c4943c7c4f7b9
Stephen Dawson <stephendawson@protonmail.com>
2023-09-19 16:40:34 +0100

    add home, contact and about templates and routes

---
+++ run.py
@@ -0,0 +1,36 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+  """
+  Displays "Hello, World" on the index page
+  """
+  return render_template('index.html')
+
+
+@app.route("/about")
+def about():
+   """
+   Displays about page
+   """
+   return render_template('about.html')
+
+
+@app.route("/contact")
+def contact():
+   """
+   Displays the contact page
+   """
+   return render_template('contact.html')
+
+
+if __name__ == "__main__":
+    app.run(
+      host=os.environ.get("IO", "0.0.0.0"),
+      port=int(os.environ.get("PORT", "5001")),
+      debug=True
+    )
+
