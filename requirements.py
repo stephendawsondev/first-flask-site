ccc3f784dd9e8626e472411f7fbaf1ed39e7589e
Stephen Dawson <stephendawson@protonmail.com>
2023-09-19 20:05:20 +0100

    add requirements.txt file

---
+++ requirements.py
@@ -0,0 +1,39 @@
+altgraph @ file:///System/Volumes/Data/SWE/Apps/DT/BuildRoots/BuildRoot7/ActiveBuildRoot/Library/Caches/com.apple.xbs/Sources/python3/python3-133.100.1.1/altgraph-0.17.2-py2.py3-none-any.whl
+autopep8==2.0.2
+blinker==1.6.2
+cachetools==5.3.1
+certifi==2023.5.7
+charset-normalizer==3.2.0
+click==8.1.7
+colorama==0.4.6
+Flask==2.3.3
+future @ file:///System/Volumes/Data/SWE/Apps/DT/BuildRoots/BuildRoot7/ActiveBuildRoot/Library/Caches/com.apple.xbs/Sources/python3/python3-133.100.1.1/future-0.18.2-py3-none-any.whl
+google-api-core==2.11.1
+google-api-python-client==2.95.0
+google-auth==2.22.0
+google-auth-httplib2==0.1.0
+google-auth-oauthlib==1.0.0
+googleapis-common-protos==1.59.1
+gspread==5.10.0
+httplib2==0.22.0
+idna==3.4
+importlib-metadata==6.8.0
+itsdangerous==2.1.2
+Jinja2==3.1.2
+macholib @ file:///System/Volumes/Data/SWE/Apps/DT/BuildRoots/BuildRoot7/ActiveBuildRoot/Library/Caches/com.apple.xbs/Sources/python3/python3-133.100.1.1/macholib-1.15.2-py2.py3-none-any.whl
+MarkupSafe==2.1.3
+oauthlib==3.2.2
+protobuf==4.23.4
+pyasn1==0.5.0
+pyasn1-modules==0.3.0
+pycodestyle==2.10.0
+pyparsing==3.1.0
+requests==2.31.0
+requests-oauthlib==1.3.1
+rsa==4.9
+six @ file:///System/Volumes/Data/SWE/Apps/DT/BuildRoots/BuildRoot7/ActiveBuildRoot/Library/Caches/com.apple.xbs/Sources/python3/python3-133.100.1.1/six-1.15.0-py2.py3-none-any.whl
+tomli==2.0.1
+uritemplate==4.1.1
+urllib3==1.26.16
+Werkzeug==2.3.7
+zipp==3.16.2
